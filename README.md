96aefb4cc780e40f7df06b204044637dfdbbfaa0
Vladas Mikalajunas <vladas.mikalajunas@adform.com>
2015-04-15 17:42:02 +0300

    Inline JS files

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
+# HTML Monolith
+
 This tool puts all js and css files into html inline.
+
+## Usage
+
+``` js
+var monolith = require('html-monolith');
+
+var filePath = '/full/path/to/file.html';
+var options = {
+    js: true,
+    css: true
+};
+
+var result = monolith.inlineSync('path/to/html/file', options);
+
+
+```
---
+++ index.js
@@ -0,0 +1,53 @@
+var fs = require('fs');
+var path = require('path');
+
+var debug = require('debug')('html-monolith');
+
+function _getMatches(string, regex) {
+    var matches = [];
+    var match;
+    while (match = regex.exec(string)) {
+        matches.push(match);
+    }
+    return matches;
+}
+
+function _trimQuotes(string) {
+    return string.replace(/^['"]+|['"]+$/gm, '');
+}
+
+var inlineSync = function (filePath, options) {
+    var dir = path.parse(filePath).dir;
+    var source = fs.readFileSync(filePath, 'utf-8');
+    var inlinedFiles = [];
+
+    var matches = _getMatches(source, new RegExp('<script.+?src=(.*?)>.*?script>', 'gi'));
+
+    matches.map(function(match){
+        var file = {
+            tag: match[0],
+            src: _trimQuotes(match[1].trim()),
+        };
+        file.path = dir + '/' + file.src;
+
+        if(!fs.existsSync(file.path)) return null;
+        var content = fs.readFileSync(file.path, 'utf-8');
+        file.content = content;
+
+        return file;
+
+    }).forEach(function(file){
+        if (!file) return;
+        source = source.split(file.tag).join('<script>' + file.content + '</script>');
+        inlinedFiles.push(file);
+    });
+
+    return {
+        source: source,
+        inlinedFiles: inlinedFiles
+    };
+};
+
+module.exports = {
+    inlineSync: inlineSync
+};
