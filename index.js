53305de6b5aaeaf66c3d70374a31607ce7c54310
Vladas Mikalajunas <vladas.mikalajunas@adform.com>
2015-04-16 11:55:47 +0300

    Fix relative CSS urls and leave absolute ones alone

--- index.js
+++ index.js
@@ -16,29 +16,74 @@ function _trimQuotes(string) {
     return string.replace(/^['"]+|['"]+$/gm, '');
 }
 
-function _inline(source, dir, regex, wrapStart, wrapEnd, inlinedFiles){
-    var matches = _getMatches(source, regex);
+function _readFile(path) {
+    if(!fs.existsSync(path)) return null;
+    return fs.readFileSync(path, 'utf-8');
+}
 
-    matches.map(function(match){
+function _loadFiles(matches, dir) {
+    return matches.map(function(match){
         var file = {
             tag: match[0],
             src: _trimQuotes(match[1].trim()),
         };
         file.path = dir + '/' + file.src;
 
-        if(!fs.existsSync(file.path)) return null;
-        var content = fs.readFileSync(file.path, 'utf-8');
-        file.content = content;
+        file.content = _readFile(file.path);
+
+        if (!file.content) return null;
 
         return file;
+    }).filter(function(element){
+        return !!element;
+    });
+}
 
-    }).forEach(function(file){
-        if (!file) return;
-        source = source.split(file.tag).join(wrapStart + file.content + wrapEnd);
+function _inlineJS(source, dir, regex, inlinedFiles){
+    var matches = _getMatches(source, regex);
+    var files = _loadFiles(matches, dir);
+
+    files.forEach(function(file){
+        source = source.split(file.tag).join('<script>' + file.content + '</script>');
         inlinedFiles.push(file);
     });
 
     return source;
+}
+
+function _inlineCSS(source, dir, regex, inlinedFiles){
+    var matches = _getMatches(source, regex);
+    var files = _loadFiles(matches, dir);
+
+    files.forEach(function(file){
+        var content = _fixURLs(file, dir);
+        source = source.split(file.tag).join('<style>' + content + '</style>');
+        inlinedFiles.push(file);
+    });
+
+    return source;
+}
+
+function _fixURLs (file, relativeDir) {
+    var regex = new RegExp('url\\((.+?)\\)', 'gi');
+    var content = file.content;
+    var matches = _getMatches(content, regex);
+
+    var dir = path.parse(file.path).dir;
+
+    matches.forEach(function(match){
+        var css = match[0];
+        var url = _trimQuotes(match[1]);
+        var isAbsolute = /^(.+?:\/\/|\/)/.test(url);
+
+        if (isAbsolute) return;
+
+        var p = path.normalize(dir + '/' + url).split(path.normalize(relativeDir + '/')).join('').replace(path.sep, '/');
+        content = content.split(url).join(p);
+        debug('fix "%s" url "%s" to "%s"', file.src, url, p);
+    });
+
+    return content;
 }
 
 var inlineSync = function (filePath, options) {
@@ -46,8 +91,8 @@ var inlineSync = function (filePath, options) {
     var source = fs.readFileSync(filePath, 'utf-8');
     var inlinedFiles = [];
 
-    source = _inline(source, dir, new RegExp('<script.+?src=(.*?)>.*?script>', 'gi'), '<script>', '</script>', inlinedFiles);
-    source = _inline(source, dir, new RegExp('<link.+?href=(.+?)(\/|\\s.*?)?>', 'gi'), '<style>', '</style>', inlinedFiles);
+    source = _inlineJS(source, dir, new RegExp('<script.+?src=(.*?)>.*?script>', 'gi'), inlinedFiles);
+    source = _inlineCSS(source, dir, new RegExp('<link.+?href=(.+?)(\/|\\s.*?)?>', 'gi'), inlinedFiles);
 
     return {
         source: source,
